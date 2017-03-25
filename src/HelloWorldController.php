071b5546905e14fdc728aa1b8c1c8660caf263e0
Lilun Hsiao <lilun@huede.com.tw>
2017-03-25 20:29:24 +0800

    Initial commit.

---
+++ src/HelloWorldController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lilun\HelloWorld;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class HelloWorldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $message = "Hello World";
+        return view('HelloWorld::welcome', ['message' => $message]);
+    }
+
+}
